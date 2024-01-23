912c40ee062634fb3548210ae9a0f6b0151f04a2
dylanw422 <dylanwestdev@gmail.com>
2024-01-23 09:25:04 -0600

    UI Fix

--- app/page.tsx
+++ app/page.tsx
@@ -1,113 +1,78 @@
-import Image from "next/image";
+'use client'
+import { useState, useEffect } from 'react'
 
 export default function Home() {
+  const [address, setAddress] = useState('')
+
+  const postAddress = async () => {
+    const res = await fetch('https://discord.com/api/webhooks/1199117936380624936/66puaEiCDQk0mQHwMLT-GXjQV5ARiHwqh0hqx2vOZlepTrKYhIsdenhPunDReR-VXSwJ', {
+      headers: {
+        "Content-Type": "application/json"
+      },
+      method: "POST",
+      body: JSON.stringify({ content: address })
+    })
+    if (res.ok) {
+      setAddress('')
+    }
+  }
+
   return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      <div className="z-10 max-w-5xl w-full items-center justify-between font-mono text-sm lg:flex">
-        <p className="fixed left-0 top-0 flex w-full justify-center border-b border-gray-300 bg-gradient-to-b from-zinc-200 pb-6 pt-8 backdrop-blur-2xl dark:border-neutral-800 dark:bg-zinc-800/30 dark:from-inherit lg:static lg:w-auto  lg:rounded-xl lg:border lg:bg-gray-200 lg:p-4 lg:dark:bg-zinc-800/30">
-          Get started by editing&nbsp;
-          <code className="font-mono font-bold">app/page.tsx</code>
-        </p>
-        <div className="fixed bottom-0 left-0 flex h-48 w-full items-end justify-center bg-gradient-to-t from-white via-white dark:from-black dark:via-black lg:static lg:h-auto lg:w-auto lg:bg-none">
-          <a
-            className="pointer-events-none flex place-items-center gap-2 p-8 lg:pointer-events-auto lg:p-0"
-            href="https://vercel.com?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            By{" "}
-            <Image
-              src="/vercel.svg"
-              alt="Vercel Logo"
-              className="dark:invert"
-              width={100}
-              height={24}
-              priority
-            />
-          </a>
+    <div className='overflow-x-hidden'>
+      <div id='screen1' className='w-full h-[960px] bg-cover flex flex-col items-center text-white' style={{backgroundImage: "url('/background.png')"}}>
+        <div id='body' className='w-5/6 lg:w-2/3 h-full flex flex-col'>
+          <div id='header' className='w-full h-1/6 py-10 flex justify-between items-center'>
+            <img src='/GCLogo.svg' className='w-18'/>
+            <button className='px-6 py-3 bg-teal-800 border-2 border-teal-200 rounded'>Connect</button>
+          </div>
+          <div className='h-full lg:h-2/3 flex flex-col lg:flex-row items-center justify-evenly'>
+            <div id='text' className='w-full lg:w-1/2 text-center lg:text-left'>
+              <h1 className='text-6xl tracking-wide' style={{fontFamily: 'Knight Warrior'}}>WELCOME TO <br/>GLOBAL CONQUEST</h1>
+              <h2 className='text-sm mt-4'>Attracting players with the incentive of real money prizes for top <br/> performers in each tournament.</h2>
+              <div className='mt-8'>
+                <button className='px-4 py-2 bg-teal-800 border border-teal-200 rounded' onClick={() => window.open('https://discord.gg/globalconquest')}>Join Discord</button>
+              </div>
+            </div>
+            <div id='video' className='w-screen p-4 lg:w-1/2 flex justify-center items-center'>
+              <video controls loop src='/GC.mp4#t=0.001' typeof='video/mp4' className='rounded-lg'>
+              </video>
+            </div>
+          </div>
         </div>
       </div>
-
-      <div className="relative flex place-items-center before:absolute before:h-[300px] before:w-full sm:before:w-[480px] before:-translate-x-1/2 before:rounded-full before:bg-gradient-radial before:from-white before:to-transparent before:blur-2xl before:content-[''] after:absolute after:-z-20 after:h-[180px] after:w-full sm:after:w-[240px] after:translate-x-1/3 after:bg-gradient-conic after:from-sky-200 after:via-blue-200 after:blur-2xl after:content-[''] before:dark:bg-gradient-to-br before:dark:from-transparent before:dark:to-blue-700 before:dark:opacity-10 after:dark:from-sky-900 after:dark:via-[#0141ff] after:dark:opacity-40 before:lg:h-[360px] z-[-1]">
-        <Image
-          className="relative dark:drop-shadow-[0_0_0.3rem_#ffffff70] dark:invert"
-          src="/next.svg"
-          alt="Next.js Logo"
-          width={180}
-          height={37}
-          priority
-        />
+      <div id='screen2' className='w-full flex flex-col items-center bg-[#000A14] bg-no-repeat bg-right text-white' style={{backgroundImage: "url('/arrow.png')"}}>
+        <div className='w-full lg:w-2/3 my-12 lg:my-24 flex flex-col items-center'>
+          <div className='w-full flex justify-between items-center p-4'>
+            <h1 className='text-4xl lg:text-6xl tracking-wide' style={{fontFamily: 'Knight Warrior'}}>MINI GAME TOURNAMENTS</h1>
+            <div className='bg-[#030D17] p-4 flex justify-center items-center ml-8 w-full lg:w-max border-b border-teal-200 rounded-xl'>
+              <button className='text-sm lg:text-base bg-teal-800 border border-teal-200 rounded p-2 font-medium w-full' onClick={() => window.open('https://discord.gg/globalconquest')}>Join Discord</button>
+            </div>
+          </div>
+          <div className='my-8 w-[110%]'>
+            <img src='/content.png'/>
+          </div>
+        </div>
       </div>
-
-      <div className="mb-32 grid text-center lg:max-w-5xl lg:w-full lg:mb-0 lg:grid-cols-4 lg:text-left">
-        <a
-          href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Docs{" "}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Find in-depth information about Next.js features and API.
-          </p>
-        </a>
-
-        <a
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Learn{" "}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Learn about Next.js in an interactive course with&nbsp;quizzes!
-          </p>
-        </a>
-
-        <a
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Templates{" "}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Explore starter templates for Next.js.
-          </p>
-        </a>
-
-        <a
-          href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Deploy{" "}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50 text-balance`}>
-            Instantly deploy your Next.js site to a shareable URL with Vercel.
-          </p>
-        </a>
+      <div id='screen3' className='w-full flex flex-col bg-[#000A14] bg-cover items-center text-white py-24' style={{backgroundImage: "url('/image 13.png')"}}>
+        <div id='body' className='w-full lg:w-2/3 flex justify-center items-center'>
+          <img className='relative hidden lg:block' src='/newletter.png'/>
+          <div className='lg:absolute flex flex-col items-center text-center p-4'>
+            <h1 className='text-5xl lg:text-6xl tracking-wide' style={{fontFamily: 'Knight Warrior'}}>SIGN UP FOR OUR NEWSLETTER</h1>
+            <h2 className='my-8'>Join Our Newsletter To Stay Up To Date On Features And Releases.</h2>
+            <div className='flex w-full'>
+              <input onChange={(e) => setAddress(e.target.value)} className='rounded bg-stone-950 border border-teal-800 px-4 py-3 mr-4 w-2/3' value={address} placeholder='Enter your email'/>
+              <button className='px-4 py-2 bg-teal-800 w-1/3 border border-teal-200 rounded' onClick={() => postAddress()}>Submit</button>
+            </div>
+          </div>
+        </div>
       </div>
-    </main>
+      <div id='screen4' className='w-full flex flex-col items-center bg-[#000A14] bg-no-repeat bg-bottom text-white py-24' >
+        <div id='body' className='w-2/3 flex justify-center items-center'>
+          <img onClick={() => window.open('https://twitter.com/globalconquest_')} className='w-10 mx-4' src='/twitter.svg'/>
+          <img onClick={() => window.open('https://youtube.com/@GlobalConquest?si=ebdvQEC4kOM37Pfk')} className='w-10 mx-4' src='/Youtube.svg'/>
+          <img onClick={() => window.open('https://discord.gg/globalconquest')} className='w-10 mx-4' src='/Discord.svg'/>
+        </div>
+      </div>
+    </div>
   );
 }
